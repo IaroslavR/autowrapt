137d908cb6916a653be401aa6b6e10db664a4135
iaroslav <iarruss@ya.ru>
2018-10-20 16:08:29 +0100

    customise for current needs

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 import sys
 import os
 
-from setuptools import setup
 from distutils.sysconfig import get_python_lib
+from setuptools import setup
 
 setup_kwargs = dict(
-    name = 'autowrapt',
-    version = '1.1',
-    description = 'Boostrap mechanism for monkey patches.',
-    author = 'Graham Dumpleton',
-    author_email = 'Graham.Dumpleton@gmail.com',
-    license = 'BSD',
-    url = 'https://github.com/GrahamDumpleton/autowrapt',
-    packages = ['autowrapt'],
-    package_dir = {'autowrapt': 'src'},
-    package_data = {'autowrapt': ['__startup__/sitecustomize.py']},
-    data_files = [(get_python_lib(prefix=''), ['autowrapt-init.pth'])],
-    entry_points = {'console_scripts': ['autowrapt = autowrapt.main:main'],
-        'autowrapt.examples': ['this = autowrapt.examples:autowrapt_this']},
-    install_requires = ['wrapt>=1.10.4'],
+    name='decimal-monkeypatch',
+    version='0.1',
+    description='Monkey patches Python 2 decimal to cdecimal',
+    author='Iaroslav Russkykh',
+    author_email='iarrus@ya.ru',
+    license='BSD',
+    url='https://github.com/IaroslavR/decimal-monkeypatch',
+    packages=['decimal-monkeypatch'],
+    package_dir={'decimal-monkeypatch': 'src'},
+    package_data={'decimal-monkeypatch': ['__startup__/sitecustomize.py']},
+    data_files=[(get_python_lib(prefix=''), ['autowrapt-init.pth'])],
+    entry_points={
+        'console_scripts': ['decimal-monkeypatch = decimal-monkeypatch.main:main'],
+        'decimal-monkeypatch.patches': ['decimal = decimal-monkeypatch.patches:autowrapt_decimal']},
+    install_requires=['wrapt>=1.10.4', 'm3-cdecimal==2.3'],
 )
 
 setup(**setup_kwargs)
