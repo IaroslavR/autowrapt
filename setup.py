b205db055e4a736430f34436bf8c95c015a3101f
iaroslav <iarruss@ya.ru>
2018-10-20 16:38:52 +0100

    [fix] setup.py

--- setup.py
+++ setup.py
@@ -6,20 +6,20 @@ from setuptools import setup
 
 setup_kwargs = dict(
     name='decimal-monkeypatch',
-    version='0.1',
+    version='0.2',
     description='Monkey patches Python 2 decimal to cdecimal',
     author='Iaroslav Russkykh',
     author_email='iarrus@ya.ru',
     license='BSD',
     url='https://github.com/IaroslavR/decimal-monkeypatch',
-    packages=['decimal-monkeypatch'],
-    package_dir={'decimal-monkeypatch': 'src'},
-    package_data={'decimal-monkeypatch': ['__startup__/sitecustomize.py']},
+    packages=['decimal_monkeypatch'],
+    package_dir={'decimal_monkeypatch': 'src'},
+    package_data={'decimal_monkeypatch': ['__startup__/sitecustomize.py']},
     data_files=[(get_python_lib(prefix=''), ['autowrapt-init.pth'])],
     entry_points={
-        'console_scripts': ['decimal-monkeypatch = decimal-monkeypatch.main:main'],
-        'decimal-monkeypatch.patches': ['decimal = decimal-monkeypatch.patches:autowrapt_decimal']},
-    install_requires=['wrapt>=1.10.4', 'm3-cdecimal==2.3'],
+        'console_scripts': ['decimal_monkeypatch = decimal_monkeypatch.main:main'],
+        'decimal_monkeypatch.patches': ['decimal = decimal_monkeypatch.patches:autowrapt_decimal']},
+    install_requires=['wrapt>=1.10.4', 'm3-cdecimal==2.3', ],
 )
 
 setup(**setup_kwargs)
