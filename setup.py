47cc76fcebb71e0ee39e076ef1aed4e14914eca9
iaroslav <iarruss@ya.ru>
2018-10-20 23:30:50 +0100

    [fix] package name in code

--- setup.py
+++ setup.py
@@ -17,8 +17,7 @@ setup_kwargs = dict(
     package_data={'decimal_monkeypatch': ['__startup__/sitecustomize.py']},
     data_files=[(get_python_lib(prefix=''), ['autowrapt-init.pth'])],
     entry_points={
-        'console_scripts': ['decimal_monkeypatch = decimal_monkeypatch.main:main'],
-        'decimal_monkeypatch.patches': ['decimal = decimal_monkeypatch.patches:autowrapt_decimal']},
+        'decimal': ['decimal = decimal_monkeypatch.patch:autowrapt_decimal']},
     install_requires=['wrapt>=1.10.4', 'm3-cdecimal==2.3', ],
 )
 
